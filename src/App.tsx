87ca88a9e9693a448966c0793564e2f052705267
Michael Del Pape <m4delpape@gmail.com>
2023-07-31 20:12:02 -0700

    initialized typescript react application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo.svg';
 import './App.css';
+import Greet from './Greet'
 
 function App() {
+  const [name, setName] = useState('"Enter Name"')
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+
+      }}>
+        <input placeholder='name' onChange={(event) => setName(event.target.value)}></input>
+
+        <button>Enter</button>
+      </form>
+      <Greet name={name}/>
     </div>
   );
 }
